bab74112d690031ed6d718b8d2006bab10d6686e
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-10 15:14:29 +0200

    [UPD] struttura statica

---
+++ src/components/Main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <>
+            <div className="container">
+                <div className='buttonMenu'>
+                    <button className='button'>HTML</button>
+                    <button className='button'>CSS</button>
+                    <button className='button'>Javascript</button>
+                    <button className='button'>Node.js</button>
+                    <button className='button'>Express</button>
+                    <button className='button'>ReactUs</button>
+                </div>
+                <div className="contentDescr">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, beatae error ipsum, fugit nisi veritatis totam deserunt eum molestiae architecto corrupti aliquam eligendi velit dolorem dolor iusto, provident repudiandae earum?</p>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Main
